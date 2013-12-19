8a04fa57441926163899cf1d7763bde3b04c1f99
Ubuntu User <ubuntu@ubuntu-armhf.(none)>
2013-12-19 19:30:42 +0000

    Updated flask structure to include sub-folder

--- src/testFlaskNode.py
+++ src/testFlaskNode.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from flask import Flask
-
-# initialize the Flask app and define routes
-app = Flask(__name__)
-testString = 'hello fuckers'
-
-@app.route('/')
-def index():
-	return testString
-
-# define the callback
-def callback(data):
-	print 'hi'
-	testString = data.data
-
+from DashApp import *
 
 if __name__ == '__main__':
+	dashApp = DashApp()
 	rospy.init_node('listener', anonymous=True)
-	rospy.Subscriber('chatter', String, callback)
-	app.run()
+	rospy.Subscriber('chatter', String, dashApp.callback)
+	dashApp.run()
