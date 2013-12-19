a87685970c8be6cea61962db645e0255fbec56eb
Ubuntu User <ubuntu@ubuntu-armhf.(none)>
2013-12-19 19:46:59 +0000

    added in controller file structure

--- src/testFlaskNode.py
+++ src/testFlaskNode.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from DashApp import *
+from DashApp import dashApp
 
 if __name__ == '__main__':
-	dashApp = DashApp()
 	rospy.init_node('listener', anonymous=True)
-	rospy.Subscriber('chatter', String, dashApp.callback)
+	rospy.Subscriber('chatter', String, dashApp.timeCallback)
 	dashApp.run()
