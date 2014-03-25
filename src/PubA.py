e4872b82bdd0c9c9aacbd2ec687adc62b07d5c1a
Dan Kearney <daniel.kearney@students.olin.edu>
2014-03-25 16:19:52 -0400

    removed ros

--- src/PubA.py
+++
@@ -1,8 +0,0 @@
-from pydispatch import dispatcher
-import time
-from random import randint
-
-def sendMessageA():
-    while True:
-        dispatcher.send(signal=randint(1,100), sender="PubA")
-        time.sleep(1)
