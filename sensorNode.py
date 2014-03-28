b42db7263e57cd19e6df49cb69a0592b0033fa69
dankearney <daniel.kearney@students.olin.edu>
2014-03-28 18:00:38 -0400

    added four sensors

--- sensorNode.py
+++ sensorNode.py
@@ -7,5 +7,31 @@ def publishFrontLeftHall():
 	dispatcher.send(signal=frontLeftHall.getSensorVal(), sender="frontLeftHall")
 frontLeftHall.setPublishFunc(publishFrontLeftHall)
 
+frontRightHall = EdgeDetector(["P8_10"], 1)
+def publishFrontLeftHall():
+	print frontRightHall
+	dispatcher.send(signal=frontRightHall.getSensorVal(), sender="frontRightHall")
+frontRightHall.setPublishFunc(publishFrontLeftHall)
+
+backLeftHall = EdgeDetector(["P8_10"], 1)
+def publishFrontLeftHall():
+	print backLeftHall
+	dispatcher.send(signal=backLeftHall.getSensorVal(), sender="backLeftHall")
+backLeftHall.setPublishFunc(publishFrontLeftHall)
+
+backRightHall = EdgeDetector(["P8_10"], 1)
+def publishFrontLeftHall():
+	print backRightHall
+	dispatcher.send(signal=backRightHall.getSensorVal(), sender="backRightHall")
+backRightHall.setPublishFunc(publishFrontLeftHall)
+
+
+
 if __name__=="__main__":
-	frontLeftHall.run()
+	from PhoenixMaster import PhoenixMaster
+	PhoenixMaster(
+		backRightHall.run,
+		frontRightHall.run,
+		backLeftHall.run,
+		backRightHall.run
+		)
