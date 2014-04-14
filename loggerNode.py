2130bdddded250799222363d70510397671caee1
root <root@bajablack7.(none)>
2014-04-14 20:23:51 +0000

    added requirements.txt and fixed printing

--- loggerNode.py
+++ loggerNode.py
@@ -1,8 +1,10 @@
 from pydispatch import dispatcher
 import pprint
 import json
+import os
 
 def logAll(sender, signal):
+	os.system('cls' if os.name == 'nt' else 'clear')
 	pprint.pprint(json.loads(signal))
 
 def run():
