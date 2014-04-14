9da7f7ac909a45b87b773c439f862c2738b5396d
root <root@bajablack7.(none)>
2014-04-14 20:31:45 +0000

    made printing stable

--- loggerNode.py
+++ loggerNode.py
@@ -1,11 +1,11 @@
 from pydispatch import dispatcher
-import pprint
 import json
 import os
 
 def logAll(sender, signal):
 	os.system('cls' if os.name == 'nt' else 'clear')
-	pprint.pprint(json.loads(signal))
+	for key, item in json.loads(signal).items():
+		print key, item
 
 def run():
 	dispatcher.connect(logAll, sender="allNode")
